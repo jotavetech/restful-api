f9c297209f8b6ec993d2d7b54d7cb8fd8b8a3161
jotavetech <ojoaogugel@gmail.com>
2024-02-29 09:39:47 -0300

    add user service

--- src/main/java/com/jotave/todolist/models/User.java
+++ src/main/java/com/jotave/todolist/models/User.java
@@ -72,6 +72,14 @@ public class User {
         this.password = password;
     }
 
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (obj == this)
---
+++ src/main/java/com/jotave/todolist/services/UserService.java
@@ -0,0 +1,52 @@
+package com.jotave.todolist.services;
+
+import com.jotave.todolist.models.User;
+import com.jotave.todolist.repositories.TaskRepository;
+import com.jotave.todolist.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    // Injeção de dependência
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    @Transactional // Garante que todas as operações sejam executadas ou nenhuma seja executada
+    public User create(User user) {
+        user.setId(null);
+        user = this.userRepository.save(user);
+        this.taskRepository.saveAll(user.getTasks());
+
+        return user;
+    }
+
+    @Transactional
+    public User update(User user) {
+        User newUser = this.findById(user.getId());
+        newUser.setPassword(user.getPassword());
+        return this.userRepository.save(newUser);
+    }
+
+    public void delete(Long id) {
+        this.findById(id);
+
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao deletar usuário");
+        }
+    }
+}
